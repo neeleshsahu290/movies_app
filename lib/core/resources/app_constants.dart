4839acb7b0bb97fa57a0bf284849df8a488326df
NeeleshSahu-at-888520271636 <Neelesh.sahu@tailnode.com>
2025-11-06 11:09:23 +0530

    api key hide

--- lib/core/resources/app_constants.dart
+++ lib/core/resources/app_constants.dart
@@ -2,6 +2,5 @@ class AppConstants {
   AppConstants._();
 
   static const int carouselSliderItemsCount = 4;
-  static const appToken =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzYyZTgzZGQ0YmQ4ZDM2MDMzYTVjNmQxNmFhNmJmYiIsIm5iZiI6MTc2MTg0NTkxNS44ODk5OTk5LCJzdWIiOiI2OTAzYTI5YmE5YTVkNjIyMGNlY2EwMjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.AP0eGcUx2nnSmy_9cfNhCZjrYTUU6Ul-MrHY1zagJaA';
+  static const appToken = "api key";
 }
